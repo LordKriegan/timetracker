20ba46565251f42dfde7e741f1014a39c582e1dc
Qamar Stationwala <YoPappy0@gmail.com>
2019-03-18 16:05:58 -0700

    trimming old code out of parser.js

--- parser/parser.js
+++ parser/parser.js
@@ -1,35 +1,3 @@
-// const parseXlsx = require('excel');
-
-// const excel2json = (file, sheets, cb) => {
-//     parseXlsx.default(file, sheets)
-//         .then((data) => {
-//             /* 
-//                 the timetrackers first 2 rows are junk data... one is the header, next are column names...
-//                 thus ive sliced them out of the array and mapped over the rest
-//             */
-//             cb(data.slice(2).map((elem) => {
-//                     return {
-//                         activityNum: elem[1],
-//                         activityDesc: elem[2],
-//                         activityTime: (parseInt(elem[3].split(":")[0]) * 60) + parseInt(elem[3].split(":")[1]),
-//                         activityPriority: elem[4].toLowerCase() || 'normal',
-//                         skipActivity: false,
-//                         completed: false
-//                         }
-//                     }
-//                 )
-//             );
-//         }
-//     );
-// }
-
-// const parseXlsx = require('excel-as-json').processFile;
-
-// const excel2json = (file, sheet, cb) => {
-//     console.log(file, sheet);
-//     parseXlsx(file, null, {sheet: sheet}, cb)
-// }   
-
 const parseXlsx = require('convert-excel-to-json');
 
 const excel2json = (file) => {
